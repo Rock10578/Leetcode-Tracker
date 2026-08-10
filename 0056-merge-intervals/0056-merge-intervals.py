402c2086f0157c5564f54373ef51cf31edb61cce
Shreyansh Patwa <84198247+Rock10578@users.noreply.github.com>
2026-08-11 03:38:11 +0530

    Time: 7 ms (71.54%), Space: 23.2 MB (28.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,14 +2,8 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         result = list()
         #SORTING THE Start TO AVOID STORE TUPLE and check for the range
+        intervals = sorted(intervals, key=lambda x: x[0])
         n = len(intervals)
-        for i in range(n-1):
-            MIN,pos = intervals[i][0],i
-            for j in range(i+1,n):
-                if intervals[j][0] < MIN:
-                    MIN,pos = intervals[j][0],j
-            if pos!=i:
-                intervals[i],intervals[pos] = intervals[pos],intervals[i]
         # NOW Start s & end e if next range lie between update s&e else change the s&e
         s,e = intervals[0][0],intervals[0][1]
         for pos in range(1,n):
